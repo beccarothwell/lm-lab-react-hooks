80f34ad5074d0caf4fe60e588eb3cee6831469c2
Becca Rothwell <5334385+beccarothwell@users.noreply.github.com>
2023-12-11 19:31:33 +0000

    feat: Implement useEffect to fetch a toDo. Store the toDo in state and display

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,41 @@
-// make an api call using useEffect
+import { useEffect, useState } from "react";
 
-export const APICall = () => {
-
-    // Your code here!
-
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
+interface ToDo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
 }
+
+// make an api call using useEffect
+export const APICall = () => {
+  const [toDo, setToDo] = useState<ToDo>();
+
+  const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+
+  useEffect(() => {
+    const fetchData = async (apiEndPoint: string) => {
+      const response = await fetch(apiEndPoint);
+      try {
+        const json = await response.json();
+        console.log(json);
+        setToDo(json as ToDo);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData(jsonTypicode);
+    console.log(toDo);
+  }, []);
+
+  return (
+    <>
+      <h2>useEffect</h2>
+      <p>UserID: {toDo?.userId}</p>
+      <p>ID: {toDo?.id}</p>
+      <p>Title: {toDo?.title}</p>
+      <p>Completed: {toDo?.completed.toString()}</p>
+    </>
+  );
+};
